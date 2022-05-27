1f7e763a4db07552b5e1af51de9630fba3892dad
Josefine Ahlstedt <josefine.ahlstedt@gmail.com>
2022-05-27 14:26:29 +0200

    Added switching turns logic

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -2,67 +2,84 @@
  * Socket Controller
  */
 
-const debug = require('debug')('battleships-server:socket_controller');
+const debug = require("debug")("battleships-server:socket_controller");
 let io = null; // socket.io server instance
-let rooms = {}
-let nextUserId = 0
-let nextRoomId = 0
-let currentRoomId = 0
-let emptyRoomExists = false
+let rooms = {};
+let nextUserId = 0;
+let nextRoomId = 0;
+let currentRoomId = 0;
+let emptyRoomExists = false;
 
 const getRoomKey = (socket) => {
 	// get the object key of the room
-	const roomOfUser = Object.values(rooms).find(room => {
-		return room.users.hasOwnProperty(socket.id)
-	})
+	const roomOfUser = Object.values(rooms).find((room) => {
+		return room.users.hasOwnProperty(socket.id);
+	});
 	if (roomOfUser) {
 		// find the id of the room the user was in
-		return Object.keys(rooms).find(key => rooms[key] === roomOfUser)
+		return Object.keys(rooms).find((key) => rooms[key] === roomOfUser);
 	}
-	return null
-}
+	return null;
+};
 
 // Handle users searching for a game
 const handleGameSearch = function () {
 	// Check for room with less than 2 users before creating a new one
-	emptyRoomExists = Object.values(rooms).find(room => Object.keys(room.users).length < 2)
+	emptyRoomExists = Object.values(rooms).find(
+		(room) => Object.keys(room.users).length < 2
+	);
 	// if the rooms object is empty or the rooms are all full, save a representation of a new room
 	if (Object.keys(rooms).length === 0 || !emptyRoomExists) {
 		// Create and join a room (this creates a proper socket.io room)
-		this.join(`game${nextRoomId}`)
+		this.join(`game${nextRoomId}`);
 		rooms[nextRoomId] = {
 			id: `game${nextRoomId}`,
-			users: {}
-		}
+			users: {},
+		};
 		// Save the current room id for later access
-		currentRoomId = nextRoomId
+		currentRoomId = nextRoomId;
 		// Add 1 to nextRoomId so that the next created room gets a unique identifier
-		nextRoomId++
-	}
-	else {
+		nextRoomId++;
+	} else {
 		// Join a room
-		this.join(`game${currentRoomId}`)
-		io.in(`game${currentRoomId}`).emit("gameFound")
+		this.join(`game${currentRoomId}`);
+		io.in(`game${currentRoomId}`).emit("gameFound");
 	}
-	debug("Specific room:", JSON.stringify(rooms[currentRoomId].users))
+	debug("Specific room:", JSON.stringify(rooms[currentRoomId].users));
 	// Save user in room representation
-	rooms[currentRoomId].users[this.id] = `user${nextUserId}`
+	rooms[currentRoomId].users[this.id] = `user${nextUserId}`;
 	// Add 1 to nextUserId so that the next socket id gets a unique identifier
-	nextUserId++
-	debug(rooms)
+	nextUserId++;
+	debug(rooms);
 	// Emit a message to the current user's room
-	io.in(`game${currentRoomId}`).emit("HiRoom")
+	io.in(`game${currentRoomId}`).emit("HiRoom");
 	// Log how many users there are in the room
-	debug("Number of users in the room:", io.sockets.adapter.rooms.get(`game${currentRoomId}`).size)
-}
+	debug(
+		"Number of users in the room:",
+		io.sockets.adapter.rooms.get(`game${currentRoomId}`).size
+	);
+
+	//Calculate wich player is first
+	console.log(rooms);
+	const id = Object.keys(rooms[currentRoomId].users);
+	console.log(id);
+
+	function getRandomInt(min, max) {
+		// min and max included
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
+	let turn = getRandomInt(0, 1);
+	console.log("The player who start is", id[turn], "Because turn is: ", turn);
+	io.in(`game${currentRoomId}`).emit("playerTurn", id[turn]);
+};
 
 // // Handle user disconnecting during a game
 // const handleUserDisconnect = (id) => {
 // 	debug(io.sockets.adapter.rooms)
 // }
 
-
-/** 
+/**
  * Export controller and attach handlers to events
  *
  */
@@ -70,50 +87,58 @@ module.exports = function (socket, _io) {
 	// save a reference to the socket.io server instance
 	io = _io;
 
-	io.on('connection', async () => {
-		debug(`User ${socket.id} connected`)
+	io.on("connection", async () => {
+		debug(`User ${socket.id} connected`);
 		// debug(`All clients: `, await io.allSockets())
-	})
+	});
 
-	socket.on('disconnecting', () => {
+	socket.on("disconnecting", () => {
 		// debug("Rooms user were in:", socket.rooms)
 		// debug("Rooms BEFORE leaving", io.sockets.adapter.rooms)
 
 		// find the key of the room the user was in
-		const idOfRoom = getRoomKey(socket)
-		debug("ID of room on disconnecting:", idOfRoom)
+		const idOfRoom = getRoomKey(socket);
+		debug("ID of room on disconnecting:", idOfRoom);
 		//Emit message to user who is left in the room
 		if (idOfRoom) {
-			io.in(rooms[idOfRoom].id).emit("userLeft", "Your opponent left.")
+			io.in(rooms[idOfRoom].id).emit("userLeft", "Your opponent left.");
 		}
 
-		debug("Room to disconnect:", rooms[idOfRoom]?.id)
+		debug("Room to disconnect:", rooms[idOfRoom]?.id);
 		// disconnect all users from the socket.io room (which deletes it)
 		if (idOfRoom) {
-			io.socketsLeave(rooms[idOfRoom].id)
+			io.socketsLeave(rooms[idOfRoom].id);
 		}
-		debug("Rooms AFTER leaving", io.sockets.adapter.rooms)
+		debug("Rooms AFTER leaving", io.sockets.adapter.rooms);
+	});
 
-	})
-
-	socket.on('disconnect', () => {
-		debug(`User ${socket.id} disconnected`)
+	socket.on("disconnect", () => {
+		debug(`User ${socket.id} disconnected`);
 		// find the key of the room the user was in
-		const idOfRoom = getRoomKey(socket)
+		const idOfRoom = getRoomKey(socket);
 		// delete the representation of the room the user was in
 		if (idOfRoom) {
-			delete rooms[idOfRoom]
+			delete rooms[idOfRoom];
 		}
 
-		debug("Rooms after deletion:", rooms)
-	})
+		debug("Rooms after deletion:", rooms);
+	});
 
-	socket.on('joinGame', handleGameSearch)
+	socket.on("joinGame", handleGameSearch);
 
-	socket.on('coordinates', (coordinates) => {
-		const idOfRoom = getRoomKey(socket)
+	socket.on("coordinates", (coordinates) => {
+		const idOfRoom = getRoomKey(socket);
 		if (idOfRoom) {
-			socket.to(rooms[idOfRoom].id).emit("coordinatesFromServer", coordinates)
+			socket
+				.to(rooms[idOfRoom].id)
+				.emit("coordinatesFromServer", coordinates);
 		}
-	})
-}
+	});
+
+	socket.on("madeMyMove", (msg) => {
+		const idOfRoom = getRoomKey(socket);
+		if (idOfRoom) {
+			socket.to(rooms[idOfRoom].id).emit("changeTurn", msg);
+		}
+	});
+};
